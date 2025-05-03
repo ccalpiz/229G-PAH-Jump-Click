8590d7664db06fbc1d16d43e7d3d090b745d8e48
ccalpiz <ccalpiz.sato@gmail.com>
2025-05-04 03:06:18 +0700

    Tileset + Character
    IDK how character disappear ;-;

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    public bool isJumping = false;
+
+    private float moveInput;
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+
+        rb2d.linearVelocity = new Vector2(moveInput * speed, rb2d.linearVelocity.y);
+
+        if (Input.GetButtonDown("Jump") && !isJumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, jumpForce));
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = false;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isJumping = true;
+        }
+    }
+}
