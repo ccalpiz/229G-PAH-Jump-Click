c97e347225772c2ae76e2e9b01055f5b47f0f5cd
ccalpiz <ccalpiz.sato@gmail.com>
2025-05-06 01:24:33 +0700

    Menu
    Finish main menu and credits page only text needed

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -8,6 +8,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("MainGame");
     }
 
+    public void OpenCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
     public void QuitGame()
     {
         Application.Quit();
