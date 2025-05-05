c2795efa3d0fbf93d81ec377f29c938a4992e35b
ccalpiz <ccalpiz.sato@gmail.com>
2025-05-05 08:09:44 +0700

    Player Hp
    Now if player hit the obstacle more then 3 times the game will restart

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,13 +1,17 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayerMovement : MonoBehaviour
+
+public class PlayerController : MonoBehaviour
 {
+    public int maxHP = 3;
     public float jumpTime = 0.5f;
     public float maxJumpSpeed = 10f;
     public float maxChargeTime = 1.5f;
     public float minChargeTime = 0.2f;
     public bool isJumping = false;
 
+    private int currentHP;
     private float chargeTime = 0f;
     private bool isCharging = false;
     private Rigidbody2D rb2d;
@@ -15,6 +19,7 @@ public class PlayerMovement : MonoBehaviour
     void Start()
     {
         rb2d = GetComponent<Rigidbody2D>();
+        currentHP = maxHP;
     }
 
     void Update()
@@ -87,6 +92,48 @@ public class PlayerMovement : MonoBehaviour
         if (other.gameObject.CompareTag("Ground"))
         {
             isJumping = false;
+
+            Vector2 stoppedVelocity = rb2d.velocity;
+            stoppedVelocity.x = 0f;
+            rb2d.velocity = stoppedVelocity;
         }
+
+        //foreach (ContactPoint2D contact in other.contacts)
+        //{
+        //    if (Vector2.Angle(contact.normal, Vector2.up) < 45f && rb2d.velocity.y <= 0f)
+        //    {
+        //        isJumping = false;
+        //        break;
+        //    }
+        //}
+
+        if (other.gameObject.CompareTag("Obstacle"))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    void TakeDamage(int amount)
+    {
+        currentHP -= amount;
+        Debug.Log("HP: " + currentHP);
+
+        if (currentHP <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player Died");
+        Invoke("RestartScene", 1f);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameObject.SetActive(false);
+    }
+
+    void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
