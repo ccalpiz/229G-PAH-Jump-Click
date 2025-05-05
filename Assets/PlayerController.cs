d507a01b6e3ac415c02d5404fcf476b9dd52dfea
ccalpiz <ccalpiz.sato@gmail.com>
2025-05-06 03:14:40 +0700

    FINISH
    Change character sprite and hp image

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -10,12 +10,17 @@ public class PlayerController : MonoBehaviour
     public float maxChargeTime = 1.5f;
     public float minChargeTime = 0.2f;
     public bool isJumping = false;
+    public Sprite jumpSprite;
+    public Sprite idleSprite;
 
     private int currentHP;
     private float chargeTime = 0f;
     private bool isCharging = false;
     private Rigidbody2D rb2d;
     private UIManager uiManager;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D characterCollider;
+
 
     void Start()
     {
@@ -24,6 +29,9 @@ public class PlayerController : MonoBehaviour
 
         uiManager = FindObjectOfType<UIManager>();
         uiManager.UpdateHP(currentHP, maxHP);
+
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        characterCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
@@ -33,6 +41,7 @@ public class PlayerController : MonoBehaviour
             Vector2 origin = rb2d.position;
             Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             Vector2 velocity = CalculateProjectileVelocity(origin, mousePos, jumpTime);
+            Vector3 scale = transform.localScale;
             rb2d.velocity = velocity;
             isJumping = true;
 
@@ -44,8 +53,40 @@ public class PlayerController : MonoBehaviour
             rb2d.velocity = velocity;
             isJumping = true;
 
-            velocity.x = Mathf.Clamp(velocity.x, -maxJumpSpeed, maxJumpSpeed);
-            velocity.y = Mathf.Clamp(velocity.y, -maxJumpSpeed, maxJumpSpeed);
+            if (mousePos.x < transform.position.x)
+            {
+                spriteRenderer.flipX = false;
+
+                FlipCollider(true);
+            }
+            else
+            {
+                spriteRenderer.flipX = true;
+
+                FlipCollider(false);
+            }
+
+            spriteRenderer.sprite = jumpSprite;
+        }
+
+        void FlipCollider(bool isFlippingLeft)
+        {
+            PolygonCollider2D polygonCollider = GetComponent<PolygonCollider2D>();
+
+            Vector2[] points = polygonCollider.points;
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                if (isFlippingLeft)
+                {
+                    points[i].x = -Mathf.Abs(points[i].x);
+                }
+                else
+                {
+                    points[i].x = Mathf.Abs(points[i].x);
+                }
+            }
+            polygonCollider.points = points;
         }
 
         //if (!isJumping)
@@ -100,6 +141,8 @@ public class PlayerController : MonoBehaviour
             Vector2 stoppedVelocity = rb2d.velocity;
             stoppedVelocity.x = 0f;
             rb2d.velocity = stoppedVelocity;
+
+            spriteRenderer.sprite = idleSprite;
         }
 
         //foreach (ContactPoint2D contact in other.contacts)
